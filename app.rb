6428009fea9458af87a2e91604312862730cc19a
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-27 00:11:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
+    "My name is __"
+  end
+  
+  get '/hometown' do
     
   end
   
-  
+  get '/favorite-song' do
+    
+  end
   
   
 end
